145e8a5c39695637840a8fa0265ec5d363bdffd6
Ilmi28 <ilmialiev28@gmail.com>
2024-03-25 16:28:13 +0100

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Stack {
+    int *stack;
+    int current_element = -1;
+    int max_size_of_stack;
+    
+    void init(int max_size){
+        max_size_of_stack = max_size;
+        stack = new int[max_size];
+    }
+    
+    void destroy(){
+        delete[] stack;
+        current_element = -1;
+    }
+    
+    void push(int element){
+        if (current_element == max_size_of_stack - 1) throw;
+        stack[++current_element] = element;
+    }
+    
+    void pop(){
+        if (current_element == -1) throw;
+        stack[current_element--] = 0;
+    }
+    
+    int top(){
+        if (current_element == -1) throw;
+        return stack[current_element];
+    }
+    
+    bool empty(){
+        if (current_element == -1) return true;
+        return false;
+    }
+    
+    bool full(){
+        if (current_element < max_size_of_stack - 1) return false;
+        return true;
+    }
+    
+};
+
+int main(){
+    Stack stack1;
+    Stack stack2;
+    stack1.init(4);
+    stack2.init(2);
+    stack1.push(1);
+    stack2.push(2);
+    cout << stack1.top();
+    cout << stack2.top();
+    
+}
