72667592adf9010aab44f7994e6ba888796b4954
JB <89127@student.pb.edu.pl>
2024-05-14 20:19:02 +0200

    Initial commit

--- program.cpp
+++ program.cpp
@@ -6,42 +6,43 @@ struct Stack {
     int *stack;
     int current_element = -1;
     int max_size_of_stack;
-    
+    //123123
+
     void init(int max_size){
         max_size_of_stack = max_size;
         stack = new int[max_size];
     }
-    
+
     void destroy(){
         delete[] stack;
         current_element = -1;
     }
-    
+
     void push(int element){
         if (current_element == max_size_of_stack - 1) throw;
         stack[++current_element] = element;
     }
-    
+
     void pop(){
         if (current_element == -1) throw;
         stack[current_element--] = 0;
     }
-    
+
     int top(){
         if (current_element == -1) throw;
         return stack[current_element];
     }
-    
+
     bool empty(){
         if (current_element == -1) return true;
         return false;
     }
-    
+
     bool full(){
         if (current_element < max_size_of_stack - 1) return false;
         return true;
     }
-    
+
 };
 
 int main(){
@@ -53,5 +54,5 @@ int main(){
     stack2.push(2);
     cout << stack1.top();
     cout << stack2.top();
-    
+
 }
